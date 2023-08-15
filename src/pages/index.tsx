e941e2eb979243fd94ecece6e1b28f1f8f897605
Anna Sudol <sudolann@gmail.com>
2023-08-15 12:35:10 +0200

    chore: Added pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import { Trade } from '@/components/Trade';
 
 export default function HomePage() {
   return (
     <Layout>
-      <Trade />
+      <p>market</p>
     </Layout>
   );
 }
